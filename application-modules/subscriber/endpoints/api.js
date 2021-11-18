48e7e3e6b05c574414981111b9e635f7a4ccb851
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-11-18 17:24:52 +0000

    Basic ping pong endpoints for subscriber modules

---
+++ application-modules/subscriber/endpoints/api.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+const { Api } = require('@s1lv3rsph3r3/metropolitan');
+/**
+ * Example usage is as follows:
+ * A simple API GET request => Api.get('/admin', (req, res) => { res.send('HELLO WORLD'); });
+ * A simple API POST request => Api.post('/admin', (req, res) => { res.send('HELLO WORLD'); });
+ */
+
+/*
+ * A simple ping test for the API to validate that the server is ready to
+ * receive messages - Only necessary for MVP for developers.
+ */
+Api.post('/ping', (req, res) => {
+  res.status(200).json('pong');
+});
---
+++ application-modules/subscriber/endpoints/web.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+const { Route } = require('@s1lv3rsph3r3/metropolitan');
+/**
+ * Example usage is as follows:
+ *
+ * A simple Web Route GET request
+ *    => Route.get('/admin', (req, res) => { res.send('HELLO WORLD'); });
+ *
+ * A simple Web Route POST request
+ *    => Route.post('/admin', (req, res) => { res.send('HELLO WORLD'); });
+ */
