11bc23189d9ce6e56156f9d7c06ee9d6055db86d
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-11-18 17:12:50 +0000

    Add subscriber bootloader

---
+++ application-modules/subscriber/bootloader.js
@@ -0,0 +1,15 @@
+/** ===================================================================== * */
+/** This is the bootloader class for the module.                          * */
+/** ===================================================================== * */
+
+/**
+ * Bootloader should call all required functions
+ * for loading necessary data for this particular
+ * module to work. Calls should be handled in a synchronous
+ * fashion to ensure the correct processes occur in
+ * declared order.
+ */
+(async () => {
+  // eslint-disable-next-line no-console
+  console.log('Start up and spawn any necessary recurring tasks here.');
+})();
