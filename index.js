cfb9eb90555630bcb586e40c1599da38ffabdc88
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-11-25 17:30:40 +0000

    Include 2 way websocket connection support

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-const environment = require('./environment');
 const { server } = require('./server');
 const expressApp = require('./app');
 const {
@@ -13,6 +12,7 @@ const { printEndpoints } = require('./utility/endpoint-utility');
 const httpServer = server.startServer(expressApp);
 
 const subscriber = require('./subscriber');
+const { bindToPort } = require('./utility/os-bind-utility');
 
 /**
  * Example of how to subscribe to a channel using a userId,domain,channel input
@@ -32,18 +32,24 @@ startApiRouting(expressApp);
 /* This should take records from the database and subscribe to the channels */
 moduleSpecificBootloader();
 
-// Let the server listen on a particular port
-httpServer.listen(environment.values().appPort, () => {
-  // eslint-disable-next-line no-console
-  console.log(
-    `Server listening on ${environment.values().appUrl} : ${
-      environment.values().appPort
-    }`,
-  );
+expressApp.use((req, res, next) => {
+  return res.status(418).json('Tea only thanks.');
 });
 
-expressApp.use((req, res, next) => {
-  return res.status(418).json("Tea only thanks.");
+// Handle IO connection here for 2 way
+// communication. This should eventually
+// be removed into another dependency.
+const io = require('socket.io')(httpServer);
+
+io.on('connection', function (socket) {
+  console.log(`${socket.id} : connected`);
+  socket.on('message', function (msg) {
+    console.log(`message : ${msg} socket : ${socket.id}`);
+  });
+  socket.on('disconnect', function () {
+    console.log(`socket disconnected : ${socket.id}`);
+  });
+  socket.emit('message', 'Hello world');
 });
 
 // TODO: Manage in a module
@@ -54,7 +60,14 @@ subscriber.on('message', (channel, message) => {
   // needs to be a suitable way to map <userId>::<domain>::<channel> to a public token
   // just like a simple string/keyword
   // this way clients connect using keyword and only get events they should
-  // io.emit(channel, message);
+  io.emit(channel, message);
 });
 
 printEndpoints();
+
+// We need an external function to
+// handle this business logic so we can mock
+// the app to test the endpoints.
+bindToPort(httpServer);
+
+module.exports = expressApp;
