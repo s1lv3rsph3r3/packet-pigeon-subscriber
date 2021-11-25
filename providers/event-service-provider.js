bda421a82c97c463ea2463153d9abea979abc7a3
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-11-25 17:26:35 +0000

    Linting of file.

--- providers/event-service-provider.js
+++ providers/event-service-provider.js
@@ -1,19 +1,21 @@
 const subscriber = require('../subscriber');
+
 class EventServiceProvider {
-    constructor() {
-        const {name} = EventServiceProvider;
-        this.subscriber = subscriber;
-        this.clazzName = name;
-        this.subscribeToChannel = this.subscribeToChannel.bind(this);
-    }
-    subscribeToChannel(userId, domain, channel) {
-        const channelName = `${userId}::${domain}::${channel}`;
-        this.subscriber.subscribe(channelName, (err, count) => {
-            // TODO: [Logging] - use appropriate logging functionality
-            console.log(`Subscribed to following channel: ${channelName}`);
-            console.log(err, count);
-        });
-    }
+  constructor() {
+    const { name } = EventServiceProvider;
+    this.subscriber = subscriber;
+    this.clazzName = name;
+    this.subscribeToChannel = this.subscribeToChannel.bind(this);
+  }
+
+  subscribeToChannel(userId, domain, channel) {
+    const channelName = `${userId}::${domain}::${channel}`;
+    this.subscriber.subscribe(channelName, (err, count) => {
+      // TODO: [Logging] - use appropriate logging functionality
+      console.log(`Subscribed to following channel: ${channelName}`);
+      console.log(err, count);
+    });
+  }
 }
 
 module.exports = new EventServiceProvider();
