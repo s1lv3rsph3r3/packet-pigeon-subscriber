4e3f0d0d804b3938c601d8d5964bff409179469e
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-11-26 15:08:41 +0000

    Update catch all test to mock redis connectivity.

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,21 +1,22 @@
 const request = require('supertest');
 const app = require('../../index');
 
-const agent = request(app);
-
 // A mock implementation of this module is in __mocks__
 jest.mock('../../utility/os-bind-utility');
+jest.mock('../../subscriber');
+
 describe('index.js => catch all', () => {
-  afterAll(() => {
-    app.close();
+  /* Setup the supertest agent for the application */
+  let agent;
+  beforeEach(() => {
+    agent = request(app);
   });
   test('should return 418 when endpoint does not exist', async () => {
-    // Arrange
-    // Act
-    // Assert
+    // Arrange + Act
     const response = await agent
       .get('/non-existent-endpoint')
       .set('Accept', 'application/json');
+    // Assert
     expect(response.statusCode).toEqual(418);
     expect(response.body).toEqual('Tea only thanks.');
   });
