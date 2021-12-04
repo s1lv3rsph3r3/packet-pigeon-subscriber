a18ef6a31be361bf43fbfdbfb89ae3e571d4a610
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-12-04 20:58:27 +0000

    Linting rules on providers

--- providers/event-service-provider.js
+++ providers/event-service-provider.js
@@ -12,7 +12,9 @@ class EventServiceProvider {
     const channelName = `${userId}::${domain}::${channel}`;
     this.subscriber.subscribe(channelName, (err, count) => {
       // TODO: [Logging] - use appropriate logging functionality
+      // eslint-disable-next-line no-console
       console.log(`Subscribed to following channel: ${channelName}`);
+      // eslint-disable-next-line no-console
       console.log(err, count);
     });
   }
